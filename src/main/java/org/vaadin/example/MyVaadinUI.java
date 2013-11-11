99edb7ab798e66e1b324a523bc431e0df2e58b0a
Matti Tahvonen <matti@vaadin.com>
2013-11-11 16:54:09 +0200

    updated to use more recent versions and incleded a test case

--- src/main/java/org/vaadin/example/MyVaadinUI.java
+++ src/main/java/org/vaadin/example/MyVaadinUI.java
@@ -1,7 +1,13 @@
 package org.vaadin.example;
 
 import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.DataSeries;
+import com.vaadin.addon.charts.model.DataSeriesItem;
+import com.vaadin.addon.charts.model.Legend;
 import com.vaadin.addon.charts.model.ListSeries;
+import com.vaadin.addon.charts.model.XAxis;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -34,6 +40,41 @@ public class MyVaadinUI extends UI
         
         chart.getConfiguration().setSeries(new ListSeries(1,2,3,4,2,3,4));
         layout.addComponent(chart);
+        // setContent(new DefectChartSizeSample());
     }
+    
+    
+    public static class DefectChartSizeSample extends VerticalLayout {
 
+        public DefectChartSizeSample() {
+            addComponent(createChart());
+            addComponent(new Button("Click Me"));
+        }
+
+        private Chart createChart(){
+            Chart chart = new Chart(ChartType.COLUMN);
+            chart.setWidth(225, Unit.PIXELS);
+            chart.setHeight(150, Unit.PIXELS);
+
+            Configuration config = chart.getConfiguration();
+
+            XAxis x = new XAxis();
+            x.setCategories("Group 1", "Group 2");
+            config.addxAxis(x);
+
+            Legend legend = new Legend();
+            legend.setEnabled(false);
+            config.setLegend(legend);
+
+            DataSeries dataSeries = new DataSeries();
+            dataSeries.add(new DataSeriesItem("Group 1", 50));
+            dataSeries.add(new DataSeriesItem("Group 2", 100));
+            config.addSeries(dataSeries);
+
+            chart.drawChart(config);
+
+            return chart;
+        }
+    }
 }
+
